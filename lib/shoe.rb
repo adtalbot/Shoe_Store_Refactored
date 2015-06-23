d58d0c726fd18bde813ad95910fd343ea51cae03
audrie <audrie.talbot>
2015-06-23 11:28:35 -0700

    Passes spec for capitalize method of shoe class.

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,3 +1,10 @@
 class Shoe < ActiveRecord::Base
   has_and_belongs_to_many(:stores)
+  before_save(:capitalize)
+
+private
+
+  define_method(:capitalize) do
+    new_name = self.name.capitalize!()
+  end
 end
--- spec/shoe_spec.rb
+++ spec/shoe_spec.rb
@@ -3,3 +3,12 @@ require('spec_helper')
 describe(Shoe) do
   it{should have_and_belong_to_many(:stores)}
 end
+
+describe(Shoe) do
+  describe('#capitalize') do
+    it('capitalizes the first letter of shoe name') do
+      test_shoe = Shoe.new({:name => 'nike'})
+      expect(test_shoe.name.capitalize()).to(eq('Nike'))
+    end
+  end
+end
